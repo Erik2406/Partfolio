207928eb48277eff59196244e51195d64687f52f
Erik2406 <129183440+Erik2406@users.noreply.github.com>
2023-03-29 14:10:42 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+const typed = new Typed(".sce-text", {
+    strings: ['Business woman', 'a  mother of four children', 'a Model'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDeplay: 1000,
+    loop: true
+});
+// --------------------------------------------------------------------------------------------------------------
+
+
